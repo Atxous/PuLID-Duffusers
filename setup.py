bea3f85ced329c8341732a2a98cd4bdeff805e5e
Edwin García <edwinleozm@gmail.com>
2024-09-24 03:46:29 -0600

    Fix setup.py
    Fix eva_clip config not included

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 
 setup(
     packages=find_packages(),
-    package_data={"eva_clip": ["bpe_simple_vocab_16e6.txt.gz"]},
+    package_data={"eva_clip": ["bpe_simple_vocab_16e6.txt.gz", "model_configs/*.json"]},
+    include_package_data=True
 )

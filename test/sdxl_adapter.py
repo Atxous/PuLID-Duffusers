e2a95fad414df4ec97c477dbd353daa3b1a91f6e
Edwin García <edwinleozm@gmail.com>
2024-09-24 18:16:55 -0600

    test adden to gitignore

--- test/sdxl_adapter.py
+++
@@ -1,6 +0,0 @@
-from pulid.pulid import PuLIDAdapter
-from diffusers import StableDiffusionXLPipeline
-
-pipe = StableDiffusionXLPipeline.from_pretrained("SG161222/RealVisXL_V4.0_Lightning")
-
-pulid_pipe = PuLIDAdapter(pipe)

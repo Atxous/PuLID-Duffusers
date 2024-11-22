523d16151819b6c0700d9682152bd493b13c3fc1
Edwin García <edwinleozm@gmail.com>
2024-11-22 02:38:15 -0600

    ip adapter compatible

--- pulid/attention.py
+++ pulid/attention.py
@@ -26,6 +26,7 @@ class AttnProcessor(nn.Module):
         pulid_ortho: str = None,
         pulid_num_zero: int = 16,
         pulid_mode:str = None,
+        **kwargs
     ):
         residual = hidden_states
 
@@ -103,13 +104,17 @@ class PuLIDAttnProcessor(torch.nn.Module):
             The number of channels in the `encoder_hidden_states`.
     """
 
-    def __init__(self, hidden_size, cross_attention_dim=None):
+    def __init__(self, original_attn_processor, hidden_size, cross_attention_dim=None):
         super().__init__()
         if not hasattr(F, "scaled_dot_product_attention"):
             raise ImportError("AttnProcessor2_0 requires PyTorch 2.0, to use it, please upgrade PyTorch to 2.0.")
+        
+        self.original_attn_processor = original_attn_processor
 
         self.id_to_k = nn.Linear(cross_attention_dim or hidden_size, hidden_size, bias=False)
         self.id_to_v = nn.Linear(cross_attention_dim or hidden_size, hidden_size, bias=False)
+
+        self.hidden_size = hidden_size
 
     def __call__(
         self,
@@ -123,71 +128,49 @@ class PuLIDAttnProcessor(torch.nn.Module):
         pulid_ortho: str = None,
         pulid_num_zero: int = 16,
         pulid_mode:str = None,
+        **kwargs
     ):
-
-        if pulid_mode is not None:
-            if pulid_mode == 'fidelity':
-                pulid_num_zero = 8
-                pulid_ortho = 'v2'
-            elif pulid_mode == 'extremely style':
-                pulid_num_zero = 16
-                pulid_ortho = 'v1'
-            else:
-                raise ValueError("Unsupported pulid mode. Use 'fidelity' or 'extremely style'.")
-            
-
-        residual = hidden_states
-
-        if attn.spatial_norm is not None:
-            hidden_states = attn.spatial_norm(hidden_states, temb)
-
-        input_ndim = hidden_states.ndim
-
-        if input_ndim == 4:
-            batch_size, channel, height, width = hidden_states.shape
-            hidden_states = hidden_states.view(batch_size, channel, height * width).transpose(1, 2)
-
-        batch_size, sequence_length, _ = (
-            hidden_states.shape if encoder_hidden_states is None else encoder_hidden_states.shape
+        original_hidden_states = self.original_processor(
+            attn=attn,
+            hidden_states=hidden_states,
+            encoder_hidden_states=encoder_hidden_states,
+            attention_mask=attention_mask,
+            temb=temb,
+            **kwargs
         )
-
-        if attention_mask is not None:
-            attention_mask = attn.prepare_attention_mask(attention_mask, sequence_length, batch_size)
-            # scaled_dot_product_attention expects attention_mask shape to be
-            # (batch, heads, source_length, target_length)
-            attention_mask = attention_mask.view(batch_size, attn.heads, -1, attention_mask.shape[-1])
-
-        if attn.group_norm is not None:
-            hidden_states = attn.group_norm(hidden_states.transpose(1, 2)).transpose(1, 2)
-
-        query = attn.to_q(hidden_states)
-
-        if encoder_hidden_states is None:
-            encoder_hidden_states = hidden_states
-        elif attn.norm_cross:
-            encoder_hidden_states = attn.norm_encoder_hidden_states(encoder_hidden_states)
-
-        key = attn.to_k(encoder_hidden_states)
-        value = attn.to_v(encoder_hidden_states)
-
-        inner_dim = key.shape[-1]
-        head_dim = inner_dim // attn.heads
-
-        query = query.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
-
-        key = key.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
-        value = value.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
-
-        # the output of sdp = (batch, num_heads, seq_len, head_dim)
-        hidden_states = F.scaled_dot_product_attention(
-            query, key, value, attn_mask=attention_mask, dropout_p=0.0, is_causal=False
-        )
-
-        hidden_states = hidden_states.transpose(1, 2).reshape(batch_size, -1, attn.heads * head_dim)
-        hidden_states = hidden_states.to(query.dtype)
 
         # for id embedding
         if id_embedding is not None:
+            if pulid_mode is not None:
+                if pulid_mode == 'fidelity':
+                    pulid_num_zero = 8
+                    pulid_ortho = 'v2'
+                elif pulid_mode == 'extremely style':
+                    pulid_num_zero = 16
+                    pulid_ortho = 'v1'
+                else:
+                    raise ValueError("Unsupported pulid mode. Use 'fidelity' or 'extremely style'.")
+
+
+            residual = hidden_states
+
+            if attn.spatial_norm is not None:
+                hidden_states = attn.spatial_norm(hidden_states, temb)
+
+            input_ndim = hidden_states.ndim
+
+            if input_ndim == 4:
+                batch_size, channel, height, width = hidden_states.shape
+                hidden_states = hidden_states.view(batch_size, channel, height * width).transpose(1, 2)
+
+            batch_size, _ , _ = (hidden_states.shape)
+            query = attn.to_q(hidden_states)
+
+            head_dim = self.hidden_size // attn.heads
+
+            query = query.view(batch_size, -1, attn.heads, head_dim).transpose(1, 2)
+
+        
             if pulid_num_zero == 0:
                 id_key = self.id_to_k(id_embedding).to(query.dtype)
                 id_value = self.id_to_v(id_embedding).to(query.dtype)
@@ -241,21 +224,25 @@ class PuLIDAttnProcessor(torch.nn.Module):
                 hidden_states = hidden_states + id_scale * orthogonal
                 hidden_states = hidden_states.to(orig_dtype)
 
-        # linear proj
-        hidden_states = attn.to_out[0](hidden_states)
-        # dropout
-        hidden_states = attn.to_out[1](hidden_states)
+            # linear proj
+            hidden_states = attn.to_out[0](hidden_states)
+            # dropout
+            hidden_states = attn.to_out[1](hidden_states)
 
-        if input_ndim == 4:
-            hidden_states = hidden_states.transpose(-1, -2).reshape(batch_size, channel, height, width)
+            if input_ndim == 4:
+                hidden_states = hidden_states.transpose(-1, -2).reshape(batch_size, channel, height, width)
 
-        if attn.residual_connection:
-            hidden_states = hidden_states + residual
+            if attn.residual_connection:
+                hidden_states = hidden_states + residual
 
-        hidden_states = hidden_states / attn.rescale_output_factor
+            hidden_states = hidden_states / attn.rescale_output_factor
 
-        return hidden_states
+            return original_hidden_states + hidden_states
+        else:
+            return original_hidden_states
 
+
+        
 
 
 def FeedForward(dim, mult=4):
--- pulid/pipelines.py
+++ pulid/pipelines.py
@@ -1,6 +1,7 @@
 from .core import PuLID, hack_unet_ca_layers
 import torch
 from .encoders import IDEncoder, IDFormer
+from .attention import PuLIDAttnProcessor
 from diffusers import (
     DiffusionPipeline,
     StableDiffusionXLPipeline,
@@ -12,6 +13,13 @@ from diffusers import (
 )
 from typing import Type, Dict
 from functools import wraps
+
+from diffusers.loaders.unet_loader_utils import  _maybe_expand_lora_scales
+from diffusers.models.attention_processor import  (
+        IPAdapterAttnProcessor,
+        IPAdapterAttnProcessor2_0
+    )
+
 
 def pipeline_creator(pipeline_constructor: Type[DiffusionPipeline]) -> Type[DiffusionPipeline]:
     
@@ -75,6 +83,74 @@ def pipeline_creator(pipeline_constructor: Type[DiffusionPipeline]) -> Type[Diff
                 }
 
             return super().__call__(*args, cross_attention_kwargs={**pulid_cross_attention_kwargs, **cross_attention_kwargs}, **kwargs )
+        
+        def set_ip_adapter_scale(self, scale):
+            """
+            Set IP-Adapter scales per-transformer block. Input `scale` could be a single config or a list of configs for
+            granular control over each IP-Adapter behavior. A config can be a float or a dictionary.
+
+            Example:
+
+            ```py
+            # To use original IP-Adapter
+            scale = 1.0
+            pipeline.set_ip_adapter_scale(scale)
+
+            # To use style block only
+            scale = {
+                "up": {"block_0": [0.0, 1.0, 0.0]},
+            }
+            pipeline.set_ip_adapter_scale(scale)
+
+            # To use style+layout blocks
+            scale = {
+                "down": {"block_2": [0.0, 1.0]},
+                "up": {"block_0": [0.0, 1.0, 0.0]},
+            }
+            pipeline.set_ip_adapter_scale(scale)
+
+            # To use style and layout from 2 reference images
+            scales = [{"down": {"block_2": [0.0, 1.0]}}, {"up": {"block_0": [0.0, 1.0, 0.0]}}]
+            pipeline.set_ip_adapter_scale(scales)
+            ```
+            """
+            unet = getattr(self, self.unet_name) if not hasattr(self, "unet") else self.unet
+            if not isinstance(scale, list):
+                scale = [scale]
+            scale_configs = _maybe_expand_lora_scales(unet, scale, default_scale=0.0)
+
+            for attn_name, attn_processor in unet.attn_processors.items():
+                
+                if isinstance(
+                    attn_processor, (IPAdapterAttnProcessor, IPAdapterAttnProcessor2_0)
+                ) or (
+                    isinstance(attn_processor, PuLIDAttnProcessor) and isinstance(attn_processor.original_processor, (
+                        IPAdapterAttnProcessor, IPAdapterAttnProcessor2_0)
+                    )
+                ): 
+                    attn_processor = attn_processor.original_processor if isinstance(
+                        attn_processor, PuLIDAttnProcessor
+                    ) and isinstance(
+                        attn_processor.original_processor, (
+                            IPAdapterAttnProcessor, IPAdapterAttnProcessor2_0
+                        )
+                    ) else attn_processor
+                    if len(scale_configs) != len(attn_processor.scale):
+                        raise ValueError(
+                            f"Cannot assign {len(scale_configs)} scale_configs to "
+                            f"{len(attn_processor.scale)} IP-Adapter."
+                        )
+                    elif len(scale_configs) == 1:
+                        scale_configs = scale_configs * len(attn_processor.scale)
+                    for i, scale_config in enumerate(scale_configs):
+                        if isinstance(scale_config, dict):
+                            for k, s in scale_config.items():
+                                if attn_name.startswith(k):
+                                    attn_processor.scale[i] = s
+                        else:
+                            attn_processor.scale[i] = scale_config
+
+
         
     return PuLIDPipeline
 
